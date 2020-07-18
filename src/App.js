957f6bb2e0c979cf96420aabf79e37f589646b45
Clément Yam <yamtzunhei@gmail.com>
2020-07-18 17:29:47 +0200

    chore: remove baseui dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
-import { Client as Styletron } from 'styletron-engine-atomic';
-import { Provider as StyletronProvider } from 'styletron-react';
-import { LightTheme, BaseProvider } from 'baseui';
-
-const engine = new Styletron();
 
 const client = new ApolloClient({
   uri: 'https://localhost:4000',
@@ -14,13 +9,9 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <StyletronProvider value={engine}>
-        <BaseProvider theme={LightTheme}>
-          <div>
-            <header>Hello World</header>
-          </div>
-        </BaseProvider>
-      </StyletronProvider>
+      <div>
+        <header>Hello World</header>
+      </div>
     </ApolloProvider>
   );
 }
